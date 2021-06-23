27659d32e2901c9fffe657165c6d19f7ee59b7b7
Liu Yang <ly_0913@foxmail.com>
2021-06-23 21:01:46 +0800

    update

--- MatlabStereoCalibration/Matlab标定/matlabstereo.m
+++ MatlabStereoCalibration/Matlab标定/matlabstereo.m
@@ -1,19 +1,30 @@
 clear;
 clc;
 
-I1 = imread('L0037.BMP');%读取左右图片
-I2 = imread('R0037.BMP');
-figure
-imshowpair(I1, I2, 'montage');
-title('Original Images');
+I1 = imread('L0005.BMP');%读取左右图片
+I2 = imread('R0005.BMP');
+%figure
+%imshowpair(I1, I2, 'montage');
+%title('Original Images');
 
 %加载stereoParameters对象。
 load('stereoParams.mat');%加载你保存的相机标定的mat
 
 [J1, J2] = rectifyStereoImages(I1, I2, stereoParams);
-figure
-imshowpair(J1, J2, 'montage');
-title('Undistorted Images');
+%figure
+%imshowpair(J1, J2, 'montage');
+%title('Undistorted Images');
 imwrite(J1,'reL.bmp');
 imwrite(J2,'reR.bmp');
+
+im_Merge = [J1,J2];%拼接图像
+figure,imshow(im_Merge);%展示图像
+hold on
+% 画水平线
+for k = 1:28
+    line([0 3908],[k*50 k*50],'color','g','LineWidth',1);
+end
+imwrite(im_Merge,'im_Merge.jpg');%保存图像
+ 
+ 
 
